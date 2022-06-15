281e12fff73f30e89cf667ca41fca37c8c341a5f
Josh Burd <Jburdt@gmail.com>
2022-06-15 13:07:02 -0400

    commit tree 820ab2f6b2d34e7bb550d60c0162b0d4087fe439

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const doger = document.getElementById('dodger');
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+        if(left > 0) {
+            doger.style.left = `${left - 1}px`;
+        }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+        if(left < 360) {
+            dodger.style.left = `${left + 1}px`
+        } 
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "Arrowleft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
